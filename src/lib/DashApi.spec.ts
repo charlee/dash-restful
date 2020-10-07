496164b16f8e9d7004a57f36b17b68fb4ecfa590
Charlee Li <oda.charlee@gmail.com>
2020-10-07 19:57:48 -0400

    added ts-jest

--- src/lib/DashApi.spec.ts
+++ src/lib/DashApi.spec.ts
@@ -1,11 +1,21 @@
-import test from 'ava';
+import DashAPI, { urlparams } from './DashApi';
 
-import { double, power } from './DashApi';
+describe('urlparams', () => {
+  test('it should construct the correct querystring', () => {
+    const params = {
+      a: 1,
+      b: '2',
+      c: [3, 4, 5],
+      d: ['a', 'b', 'c'],
+      e: null,
+      f: undefined,
+      g: '',
+      'h+i': 'h+i',
+    };
 
-test('double', (t) => {
-  t.is(double(2), 4);
+    const qs = urlparams(params);
+    expect(qs).toEqual('a=1&b=2&c=3%2C4%2C5&d=a%2Cb%2Cc&h%2Bi=h%2Bi');
+  });
 });
 
-test('power', (t) => {
-  t.is(power(2, 4), 16);
-});
+describe('DashAPI', () => {});
